80a44fedc63d09ae33376bd5d34b98e0e0efd220
lweicdsor <lweicdsor@gmail.com>
2017-08-17 14:15:54 -0500

    Update OSTSC.R
    add more input error checking

--- R/OSTSC.R
+++ R/OSTSC.R
@@ -10,10 +10,12 @@
 #' @param target_class the label of the class which need to be oversampled
 #' @param ratio targeted positive samples number to achieve/negative samples number, with the default value 1
 #' @param Per Percentage of the mixing between SPO and ADASYN, with the default value 0.8
-#' @param R An scalar ratio to tell in which level (towards the boundary) we shall push our syntactic data, 
+#' @param R An scalar ratio to tell in which level (towards the boundary) we shall push our syntactic data in EPSO, 
 #'          with the default value 1
 #' @param k k-NN used in the ADASYN algorithm, with the default value 5
 #' @param m m-NN used in ADASYN, finding seeds from the Positive Class, with the default value 15
+#' @param parallel Whether to run in parallel, with the default setting TRUE
+#' @param progBar When running in parallel, whether to include progress bars, with the default setting TRUE
 #' @return sample: the time series sequences data oversampled
 #' @return label: the label corresponding to each row of records
 #' @importFrom stats na.omit 
@@ -31,7 +33,7 @@
 #' # print the label data after oversampling
 #' MyData$label
 
-OSTSC <- function(sample, label, target_class, ratio=1, Per=0.8, R=1, k=5, m=15) {
+OSTSC <- function(sample, label, target_class, ratio=1, Per=0.8, R=1, k=5, m=15, parallel=TRUE, progBar=TRUE) {
   # Oversample a time series sequence imbalance data.
   #
   # Args:
@@ -40,9 +42,9 @@ OSTSC <- function(sample, label, target_class, ratio=1, Per=0.8, R=1, k=5, m=15)
   #   target_class: the label of the class which need to be oversampled.
   #   ratio:        targeted positive samples number to achieve/negative samples number, 
   #                 with the default value 1.
-  #   Per:          Percentage of the mixing between SPO and ADASYN, with the default value 0.8.
+  #   Per:          Percentage of the mixing between EPSO and ADASYN, with the default value 0.8.
   #   R:            An scalar ratio to tell in which level (towards the boundary) we shall push our 
-  #                 syntactic data, with the default value 1.
+  #                 syntactic data in EPSO, with the default value 1.
   #   k:            k-NN used in the ADASYN algorithm, with the default value 5.
   #   m:            m-NN used in ADASYN, finding seeds from the Positive Class, with the default value 15.
   #
@@ -66,10 +68,93 @@ OSTSC <- function(sample, label, target_class, ratio=1, Per=0.8, R=1, k=5, m=15)
     stop ("Number of time series sequences provided in sample do not match the number of classes provided in label.")
   }
   
+  # check if the target_class input is in the numeric format
+  if (!is.numeric(target_class)) {
+    stop ("The parameter target_class is not in correct format, which must be a numeric value.")
+  }  
+  
   # check if the target_class input is only one element
   if (length(target_class) != 1) {
-    stop ("The target_class is not in correct format, which must be a single value.")
+    stop ("The parameter target_class is not in correct format, which must be a single value.")
   }
+  
+  # check if the ratio input is in the numeric format
+  if (!is.numeric(ratio)) {
+    stop ("The parameter ratio is not in correct format, which must be a numeric value.")
+  }    
+  
+  # check if the ratio input is in range (0,1]
+  if (ratio > 1 || ratio <= 0) {
+    stop ("The parameter ratio is not in correct range, which must be betwwen 0 to 1, including 1.")
+  }
+  
+  # check if the Percentage input is in the numeric format
+  if (!is.numeric(Per)) {
+    stop ("The parameter Per is not in correct format, which must be a numeric value.")
+  }  
+  
+  # check if the Percentage input is in range [0,1]
+  if (Per > 1 || Per < 0) {
+    stop ("The parameter Per is not in correct range, which must be betwwen 0 to 1, including 0 and 1.")
+  }
+  
+  # check if the R input is in the numeric format
+  if (!is.numeric(R)) {
+    stop ("The parameter R is not in correct format, which must be a numeric value.")
+  } 
+  
+  # check if the R input is in range [1,+oo)
+  if (R < 1) {
+    stop ("The parameter R is not in correct range, which must be larger or equal to 1.")
+  }
+  
+  # check if the k input is in the numeric format
+  if (!is.numeric(k)) {
+    stop ("The parameter k is not in correct format, which must be a numeric value.")
+  }  
+  
+  # check if the k input is an integer
+  if (k%%1 != 0) {
+    stop ("The parameter k is not in correct format, which must be an integer.")
+  }
+
+  # check if the k input is in range (0,+oo)
+  if (k <= 0) {
+    stop ("The parameter k is not in correct range, which must be larger than 0.")
+  }
+  
+  # check if the m input is in the numeric format
+  if (!is.numeric(m)) {
+    stop ("The parameter m is not in correct format, which must be a numeric value.")
+  }  
+  
+  # check if the m input is an integer
+  if (m%%1 != 0) {
+    stop ("The parameter m is not in correct format, which must be an integer.")
+  }
+
+  # check if the m input is in range (0,+oo)
+  if (m <= 0) {
+    stop ("The parameter m is not in correct range, which must be larger than 0.")
+  }
+  
+  # check if the parallel input is a boolean value
+  if (!(identical(parallel, FALSE) || identical(parallel, TRUE))) {
+    stop ("The parameter parallel is not in correct format, which must be a boolean value.")
+  }
+  
+  # check if the progBar input is a boolean value
+  if (!(identical(progBar, FALSE) || identical(progBar, TRUE))) {
+    stop ("The parameter progBar is not in correct format, which must be a boolean value.")
+  }
+  
+  # check if the progBar input is TRUE when parallel input is FALSE
+  if (identical(parallel, FALSE) && identical(progBar, TRUE)) {
+    warning ("The progress bar is available only when the function running in parallel. 
+              Because the parameter parallel set to FALSE, progBar set to FALSE too.")
+    progBar = FALSE
+  }
+  
   
   fullData <- cbind(label, sample)
   fullData <- matrix(unlist(fullData, use.names = FALSE), ncol = ncol(fullData))
