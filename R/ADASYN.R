83bf8633dd2621898179858dbe8a777dd17b2923
lweicdsor <lweicdsor@gmail.com>
2017-08-20 18:54:33 -0500

    Update ADASYN.R

--- R/ADASYN.R
+++ R/ADASYN.R
@@ -2,7 +2,7 @@
 #' 
 #' @param P minority class samples
 #' @param N majority class samples
-#' @param nTarget The targeted number of samples to achieve
+#' @param nTarget the targeted number of samples to achieve
 #' @param k k-NN used in the ADASYN algorithm, with the default value 5
 #' @param m m-NN used in ADASYN, finding seeds from the Positive Class, with the default value 15
 #' @return sample_ada
@@ -23,7 +23,7 @@ ADASYN <- function(P, N, nTarget, k, m) {
   #
   # Returns:
   #   The ADASYN oversampled dataset sample_ada.
-  NT <- ncol(P)  # NT is number of samples in P
+  NT <- ncol(P)  # number of samples in positive data
   if (NT == 0) {
     stop ("The minority class is empty")
   } else if (NT == 1) {
@@ -34,10 +34,11 @@ ADASYN <- function(P, N, nTarget, k, m) {
       warning ("The minority class instances is not enough. k is set to ", k)
     } 
     NumAtt <- nrow(P)  # Feature dimension
-    ratio <- FindRatio(P, N, m)
-    No <- round(nTarget*ratio)
-    while (sum(No) != nTarget) {
-      tmp <- max(No)
+    ratio <- FindRatio(P, N, m)  # the ratio of each positive sample need to be duplicated
+    No <- round(nTarget*ratio)  # the number of each positive sample need to be duplicated
+    # adjust No to make the total number of new created samples to equal to the number needed
+    while (sum(No) != nTarget) {  
+      # tmp <- max(No)
       ind <- which.max(No)
       diff <- nTarget - sum(No)
       if (No[ind] + diff > 0) {
@@ -46,31 +47,31 @@ ADASYN <- function(P, N, nTarget, k, m) {
         No[ind] <- 0
       }
     }
-    # generation
+    # data generation
     sample_ada <- list()
     for (i in 1:length(No)) {
-      if (No[i] == 0) {
+      if (No[i] == 0) {  # jump the positive samples which don't need to be duplicated
         next
       }
       # k-NN
-      d <- rdist(t(P[, i]), t(P))
+      d <- rdist(t(P[, i]), t(P))  # the Euclidean distance between each positive sample and other positive data
       d[i] <-Inf  # Set d[i] to infinity manually
       # Find the k indices corresponding to the closest indices
       if (k<log(NT)) {
         min_id <- list()
         for (j in 1:k) {
-          tmp <- min(d)
+          # tmp <- min(d)
           id <- which.min(d)
           d[id] <-Inf
           min_id <- cbind(min_id, id)  # sort>=O(n*logn),so we take min: O(n).total time:O(k*n)
         } 
       }else {
-        tmp <- sort(d)
+        # tmp <- sort(d)
         id <- order(d)
         min_id <- id[1:k]
       }
       
-      rn <- floor(runif(No[i], min=0, max=k)) + 1
+      rn <- floor(runif(No[i], min=0, max=k)) + 1  # random generated No[i] elements integer vector in range 1 to k
       id <- min_id[rn]
       weight <- matrix(runif(NumAtt*No[i]), nrow=NumAtt, ncol=No[i], byrow = TRUE)
       D <- kronecker(matrix(1, 1, No[i]), P[, i])
