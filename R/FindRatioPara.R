8f519b124b6351b2e6ea98e583a79f6a75997b32
lweicdsor <lweicdsor@gmail.com>
2017-08-20 17:46:41 -0500

    update comments

--- R/FindRatioPara.R
+++ R/FindRatioPara.R
@@ -20,16 +20,17 @@ FindRatioPara <- function(P, N, m) {
   #   m: m-NN used in finding seeds from the Positive Class, with the default value 15
   #
   # Returns:
-  #   The ratio of each positive sample need to be duplicated ratio.
+  #   ratio: the ratio of each positive sample need to be duplicated.
   C <- cbind(P, N)  # Note that here columns of P and N are the samples
-  poscnt <- ncol(P)
+  poscnt <- ncol(P)  # Number of positive records
   
-  cl <- makeCluster(detectCores(logical = FALSE) - 1)
+  #   The ratio of each positive sample need to be duplicated
+  cl <- makeCluster(detectCores(logical = FALSE) - 1)  # start parallel
   registerDoParallel(cl, cores = cores)
-
   seq <- foreach(i = 1:poscnt, .combine = 'rbind') %dopar% {
-    d <- rdist(t(P[, i]), t(C))
+    d <- rdist(t(P[, i]), t(C))  # the Euclidean distance between each positive sample and full data
     d[i] <- Inf
+    # find m number of smallest elements from the Euclidean distance between each positive sample and full data
     min_id <- matrix(0, m, 1)
     for (j in 1:m) {
       tmp <- min(d)
@@ -37,14 +38,11 @@ FindRatioPara <- function(P, N, m) {
       d[id] <- Inf
       min_id[j] <- id  # sort>=O(n*logn),so we take min: O(n).total time:O(k*n)
     }
-    Ind <- which(min_id > poscnt)
+    Ind <- which(min_id > poscnt)  # find the number of this m number elements larger than the number of positive records
     return(length(Ind))
-  }
-  
-  stopCluster(cl)
-  
+  }  
+  stopCluster(cl)  # end parallel  
   ratio <- matrix(unlist(seq), ncol = 1, byrow = TRUE)
-
   ratio <- ratio/sum(ratio)
   
   return(ratio)
